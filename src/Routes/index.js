a059afc71dd0850feb259302d64c7bb9ed17d648
Galvs135 <lg_franca@hotmail.com>
2022-06-20 20:10:55 -0300

    provider: signUp - Criado e testado provider responsavel pela criacao de usuario

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,15 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "../Pages/Home";
+import { SignUp } from "../Pages/SignUp";
 
 export const RoutesComponent = () => {
-    return <Routes>
-        <Route path="/" element={<Home/>} ></Route>
-        <Route path="/:id"></Route>
-        <Route path="/main"></Route>
-        <Route path="/login"></Route>
-        <Route path="/signUp"></Route>
-        <Route path="/aboutUs"></Route>
-        <Route path="/groups"></Route>
-        <Route path="/groups/:id"></Route>
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/:id"></Route>
+      <Route path="/main"></Route>
+      <Route path="/login"></Route>
+      <Route path="/signUp" element={<SignUp />}></Route>
+      <Route path="/aboutUs"></Route>
+      <Route path="/groups"></Route>
+      <Route path="/groups/:id"></Route>
     </Routes>
-}
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-import { GlobalStyle } from './Styles/GlobalStyle';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'
-import { Providers } from './Providers';
-import { RoutesComponent } from './Routes';
+import { GlobalStyle } from "./Styles/GlobalStyle";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Providers } from "./Providers";
+import { SignUpProvider } from "./Providers/SignUp";
+import { RoutesComponent } from "./Routes";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Providers>
-      <BrowserRouter>
-        <GlobalStyle/>
-        <RoutesComponent/>
-      </BrowserRouter>
+      <SignUpProvider>
+        <BrowserRouter>
+          <GlobalStyle />
+          <RoutesComponent />
+        </BrowserRouter>
+      </SignUpProvider>
     </Providers>
   </React.StrictMode>
 );
