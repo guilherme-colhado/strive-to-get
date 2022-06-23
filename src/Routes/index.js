e0cdbff12b9b50a2de22702ca3f68793581f5621
ibarbarasouza <ibarbarasouza@icloud.com>
2022-06-23 17:21:49 -0300

    feat: Info card com listas de informações e benefícios do site

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,15 +1,16 @@
 import { Routes, Route } from "react-router-dom";
-import { Home } from "../Pages/Home";
 
 export const RoutesComponent = () => {
-    return <Routes>
-        <Route path="/" element={<Home/>} ></Route>
-        <Route path="/:id"></Route>
-        <Route path="/main"></Route>
-        <Route path="/login"></Route>
-        <Route path="/signUp"></Route>
-        <Route path="/aboutUs"></Route>
-        <Route path="/groups"></Route>
-        <Route path="/groups/:id"></Route>
+  return (
+    <Routes>
+      <Route path="/"></Route>
+      <Route path="/:id"></Route>
+      <Route path="/main"></Route>
+      <Route path="/login"></Route>
+      <Route path="/signUp"></Route>
+      <Route path="/aboutUs"></Route>
+      <Route path="/groups"></Route>
+      <Route path="/groups/:id"></Route>
     </Routes>
-}
+  );
+};
