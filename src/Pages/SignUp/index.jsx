70d809f699273bdb3d6e9f77c38d4970be831d0e
Wallace Junior <wallaceoliveirarn@gmail.com>
2022-06-24 17:33:48 -0300

    feat: SignUp criacao do componente signup

--- src/Pages/SignUp/index.jsx
+++ src/Pages/SignUp/index.jsx
@@ -1,3 +1,25 @@
+import { Container } from "@mui/material";
+import "./style.css";
+import { useForm } from "react-hook-form";
+
 export const SignUp = () => {
-    return
-}
+  const { register, handleSubmit } = useForm();
+
+  const sendData = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <Container>
+      <h2> Cadastre-se</h2>
+      <form onSubmit={() => handleSubmit(sendData)}>
+        <input placeholder="username" {...register("username")} />
+        <input placeholder="Email" {...register("email")} />
+        <input placeholder="Password" {...register("password")} />
+        <button type="submit">Sign Up</button>
+      </form>
+    </Container>
+  );
+};
+
+export default SignUp;
