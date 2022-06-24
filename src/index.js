853d085fc2836e35991ae5af3895599d577481f4
guilherme <85708476+guilherme-colhado@users.noreply.github.com>
2022-06-24 01:31:57 -0300

    fix: Developer arrumando o toastify

--- src/index.js
+++ src/index.js
@@ -5,23 +5,14 @@ import { BrowserRouter } from 'react-router-dom'
 import { Providers } from './Providers';
 import { RoutesComponent } from './Routes';
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Providers>
       <BrowserRouter>
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
+        <ToastContainer/>
         <GlobalStyle/>
         <RoutesComponent/>
       </BrowserRouter>
