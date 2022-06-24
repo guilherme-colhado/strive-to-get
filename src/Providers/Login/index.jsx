e10476148f37572d511d8615c52e9bc468a61b38
guilherme <85708476+guilherme-colhado@users.noreply.github.com>
2022-06-24 02:10:00 -0300

    feat: Login pagina criada

--- src/Providers/Login/index.jsx
+++ src/Providers/Login/index.jsx
@@ -1,22 +1,20 @@
 import { createContext } from "react";
 import { Api } from "../../Services/api";
-
+import { toast } from "react-toastify";
+import { useState } from "react";
+import { useEffect } from "react";
+import { Navigate } from "react-router-dom";
 export const LoginContext = createContext();
 
 export const LoginProvider = ({ children }) => {
+  const [token, setToken] = useState('');
   const Login = (data) => {
-    console.log(data);
     Api.post("/sessions/", data)
-      .then((res) =>
-        localStorage.setItem(
-          "@StriveToGet: Token",
-          JSON.stringify(res.data.access)
-        )
-      )
-      .catch((res) => console.log("Resposta a ser dada no toast"));
+      .then((res) =>setToken(res.data.access))
+      .catch((res) => toast.error('Não foi possivel logar, tente novamente!!'));
   };
 
   return (
-    <LoginContext.Provider value={{ Login }}>{children}</LoginContext.Provider>
+    <LoginContext.Provider value={{ Login, token }}>{children}</LoginContext.Provider>
   );
 };
