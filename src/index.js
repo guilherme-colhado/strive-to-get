e5d78c14c0f91a912d0ad84b304379b91c61efcd
edson vinicius <edsonvin1801@gmail.com>
2022-06-25 17:27:54 -0300

    feat: add habits provider

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import { GlobalStyle } from './Styles/GlobalStyle';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'
-import { Providers } from './Providers';
-import { RoutesComponent } from './Routes';
+import { GlobalStyle } from "./Styles/GlobalStyle";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Providers } from "./Providers";
+import { RoutesComponent } from "./Routes";
+import AboutUs from "./Components/AboutUs";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Providers>
       <BrowserRouter>
-        <GlobalStyle/>
-        <RoutesComponent/>
+        <GlobalStyle />
+        <RoutesComponent />
       </BrowserRouter>
     </Providers>
   </React.StrictMode>
