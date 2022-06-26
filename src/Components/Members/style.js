c99d333ee089b58e5e5ef41ba57300ec5e789199
guilherme <85708476+guilherme-colhado@users.noreply.github.com>
2022-06-26 19:18:54 -0300

    feat: Members criado

---
+++ src/Components/Members/style.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const MembersComponent = styled.div`
+    padding: 10px;
+    margin: 0 auto;
+    background-color: var(--CardDark);
+    border-radius: 10px;
+    width: max-content;
+    max-width: 320px;
+    ul{ 
+        margin-left: 15px;
+        color: white;
+        padding-bottom: 10px;
+        font-size: 2rem;
+        list-style: circle;
+    }
+    li{
+        margin-top: 10px;
+    }
+
+`
