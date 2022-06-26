0e084efbbc7b53c0d14581233da296843afde514
Fernanda Constância <91707875+fernandaconstancia@users.noreply.github.com>
2022-06-26 19:53:34 -0300

    feat: ComfirmAchieved criação do component arquivar

---
+++ src/Components/ComfirmAchieved/index.jsx
@@ -0,0 +1,19 @@
+import * as S from "./style";
+
+export const ComfirmAchieved = ({ onClose, onAchieved }) => {
+  return (
+    <S.Container>
+      <S.Box>
+        <p>Você tem certeza que quer arquivar este hábito?</p>
+        <S.BoxButton>
+          <S.ButtonNot onclick={onClose}>
+            <span>Ainda não</span>
+          </S.ButtonNot>
+          <S.ButtonYes onclick={onAchieved}>
+            <span>Arquivar</span>
+          </S.ButtonYes>
+        </S.BoxButton>
+      </S.Box>
+    </S.Container>
+  );
+};
