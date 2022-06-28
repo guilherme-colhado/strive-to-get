ded74a128d730b87013ffd01f79a91007046d07e
guilherme <85708476+guilherme-colhado@users.noreply.github.com>
2022-06-28 19:20:22 -0300

    feat: Developer inserindo inputs no global

--- src/Styles/GlobalStyle.js
+++ src/Styles/GlobalStyle.js
@@ -13,7 +13,7 @@ export const GlobalStyle =  createGlobalStyle`
     article, aside, canvas, details, embed, 
     figure, figcaption, footer, header, hgroup, 
     menu, nav, output, ruby, section, summary,
-    time, mark, audio, video {
+    time, mark, audio, video, input, select, text-area {
         margin: 0;
         padding: 0;
         vertical-align: baseline;
