d386a4a97c7238f190a85f6498da4da4ffd998b8
guilherme <85708476+guilherme-colhado@users.noreply.github.com>
2022-06-29 01:35:41 -0300

    refactor:  Groups retirando o GroupsComponent do arquivo index no pages Login

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,10 +1,8 @@
 import { Header } from '../../Components/Header'
 import { Login } from '../../Components/Login'
-import { GroupsComponent } from '../../Components/Groups'
 export const LoginComponent = () => {
   return <>
     <Header/>
-    <GroupsComponent/>
     <main>
       <Login></Login>
     </main>
