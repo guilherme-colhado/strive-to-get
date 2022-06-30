60a898b7f8778d5bb5ed2e10f4c9076cc4f000e0
Galvs135 <lg_franca@hotmail.com>
2022-06-30 19:30:17 -0300

    feat: feature-CardGroup   Criado cada card dos grupos.

--- src/Providers/groups/index.jsx
+++ src/Providers/groups/index.jsx
@@ -1,55 +1,64 @@
 import { createContext, useState, useEffect } from "react";
 import { Api } from "../../Services/api";
 import { toast } from "react-toastify";
+import { get } from "react-hook-form";
 
 export const GroupsContext = createContext();
 
 export const GroupsProvider = ({ children }) => {
-  const [groups, setGroups] = useState({});
+  const [groups, setGroups] = useState([]);
   const [group, setGroup] = useState({});
+  const [page, setPage] = useState(1);
 
-  const listGroups = () => {
-    Api.get("groups/")
-      .then((response) => setGroups(response.data))
+  const Get = (PerPage) => {
+    Api.get(`/groups/?page=${PerPage}`)
+      .then((response) => setGroups((pv) => [...pv, ...response.data.results]))
       .catch((err) => console.log(err));
   };
 
+  const PerPage = () => {
+    for (let i = 0; i < 2; i++) {
+      Get(page + i);
+    }
+    setPage(page + 1);
+  };
+
   useEffect(() => {
-    listGroups();
+    PerPage();
   }, []);
 
   const listOneGroup = (group) => {
-    Api.get(`groups/${group.id}/`)
+    Api.get(`/groups/${group.id}/`)
       .then((response) => setGroup(response.data))
       .catch((err) => console.log(err));
   };
 
   const createGroup = (data) => {
-    Api.post("groups/", data)
+    Api.post("/groups/", data)
       .then(toast.success("Group Created!"))
       .catch((err) => console.log(err));
   };
 
   const editGroup = (data) => {
-    Api.patch(`groups/${data.id}/`, data)
+    Api.patch(`/groups/${data.id}/`, data)
       .then(toast.success("Group edited!"))
       .catch((err) => console.log(err));
   };
 
   const groupSubscription = (data) => {
-    Api.post(`groups/${data.id}/subscribe/`)
+    Api.post(`/groups/${data.id}/subscribe/`)
       .then(toast.success("Subscribed!"))
       .catch((err) => console.log(err));
   };
 
   const buscaSubs = () => {
-    Api.get(`groups/subscriptions/`)
+    Api.get(`/groups/subscriptions/`)
       .then(toast.success("Subed Groups"))
       .catch((err) => console.log(err));
   };
 
   const exitGroup = (group) => {
-    Api.delete(`groups/${group.id}/unsubscribe/`)
+    Api.delete(`/groups/${group.id}/unsubscribe/`)
       .then(toast.success("Unsubscribed sucessufully"))
       .catch((err) => console.log(err));
   };
@@ -59,7 +68,7 @@ export const GroupsProvider = ({ children }) => {
       value={{
         groups,
         group,
-        listGroups,
+        PerPage,
         listOneGroup,
         createGroup,
         editGroup,
