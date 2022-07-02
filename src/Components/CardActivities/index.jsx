439015a287e1125764c7f955694d958aa3548076
edson vinicius <edsonvin1801@gmail.com>
2022-07-02 15:31:20 -0300

    feat: add cardActivities

---
+++ src/Components/CardActivities/index.jsx
@@ -0,0 +1,14 @@
+import { CardStyle, HeaderCard, InfosCard } from "./style";
+
+export const CardActivities = () => {
+  return (
+    <CardStyle>
+      <HeaderCard>Activities</HeaderCard>
+      <InfosCard>
+        <div>
+          
+        </div>
+      </InfosCard>
+    </CardStyle>
+  );
+};
--- src/Routes/index.js
+++ src/Routes/index.js
@@ -5,18 +5,20 @@ import { Groups } from "../Pages/Groups";
 import { Home } from "../Pages/Home";
 import { LoginComponent } from "../Pages/Login";
 import { Main } from "../Pages/Main";
+import { CardActivities } from "../Components/CardActivities";
 
 export const RoutesComponent = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/:id"></Route>
-      <Route path="/main" element={<Main/>}></Route>
+      <Route path="/main" element={<Main />}></Route>
       <Route path="/login" element={<LoginComponent />}></Route>
       <Route path="/signUp"></Route>
       <Route path="/aboutUs"></Route>
       <Route path="/groups" element={<Groups />}></Route>
       <Route path="/groups/:id"></Route>
+      <Route path="/teste" element={<CardActivities/>}></Route>
     </Routes>
   );
 };
