53e67eaaa4e09411b047bb8ef63fda733fb9dbab
edson vinicius <edsonvin1801@gmail.com>
2022-07-02 14:58:30 -0300

    feat: add imgs

--- src/Components/AboutUs/style.js
+++ src/Components/AboutUs/style.js
@@ -24,6 +24,18 @@ export const Container = styled.div`
     text-decoration: none;
     color: white;
   }
+
+  img {
+    width: 120px;
+    height: 120px;
+    margin: 20px;
+    margin-bottom: 0px;
+    border-radius: 50%;
+  }
+
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
 `;
 
 export const DevBoxLeft = styled.div`
@@ -31,13 +43,22 @@ export const DevBoxLeft = styled.div`
   flex-direction: column;
   justify-content: center;
   background-color: var(--CardDark);
-  height: 250px;
+  height: 300px;
   width: 700px;
   border-radius: 15px;
   margin: 20px 0 10px -450px;
+  text-align: justify;
+  border: 2px solid var(--BtnDark);
 `;
 export const DevBoxRight = styled(DevBoxLeft)`
   margin-left: 450px;
+`;
+
+export const ImgName = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 0;
 `;
 
 export const InfoDev = styled.div`
--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import AboutUs from "../Pages/AboutUs";
+import AboutUs from "../Components/AboutUs";
 
 export const RoutesComponent = () => {
   return (
@@ -9,7 +9,7 @@ export const RoutesComponent = () => {
       <Route path="/main"></Route>
       <Route path="/login"></Route>
       <Route path="/signUp"></Route>
-      <Route path="/aboutUs" element={<AboutUs/>}></Route>
+      <Route path="/aboutUs" element={<AboutUs />}></Route>
       <Route path="/groups"></Route>
       <Route path="/groups/:id"></Route>
     </Routes>
