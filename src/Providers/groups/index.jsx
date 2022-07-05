218e4a02072d66c36d6db8a0938701f50d2888fd
edson vinicius <edsonvin1801@gmail.com>
2022-07-05 19:55:39 -0300

    fix: add authorization

--- src/Providers/groups/index.jsx
+++ src/Providers/groups/index.jsx
@@ -1,72 +1,121 @@
 import { createContext, useState, useEffect } from "react";
 import { Api } from "../../Services/api";
 import { toast } from "react-toastify";
+import { get } from "react-hook-form";
+import jwt_decode from "jwt-decode";
 
 export const GroupsContext = createContext();
 
 export const GroupsProvider = ({ children }) => {
-  const [groups, setGroups] = useState({});
+  const [groups, setGroups] = useState([]);
+  const [groupsInscribed, setGroupsInscribed] = useState([]);
   const [group, setGroup] = useState({});
+  const [token, setToken] = useState(
+    JSON.parse(localStorage.getItem("@StriveToGet: Token")) || ""
+  );
 
-  const listGroups = () => {
-    Api.get("groups/")
-      .then((response) => setGroups(response.data))
+  let user_id = token ? jwt_decode(token).user_id : "";
+
+  const Get = (PerPage) => {
+    Api.get(`/groups/?page=${PerPage}`)
+      .then((response) => setGroups((pv) => [...pv, ...response.data.results]))
       .catch((err) => console.log(err));
   };
 
+  const PerPage = (x) => {
+    for (let i = x; i < x + 2; i++) {
+      Get(i);
+    }
+  };
+
   useEffect(() => {
-    listGroups();
+    PerPage(1);
   }, []);
 
   const listOneGroup = (group) => {
-    Api.get(`groups/${group.id}/`)
+    Api.get(`/groups/${group.id}/`, {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(
+          localStorage.getItem("@StriveToGet: Token" || "")
+        )}`,
+      },
+    })
       .then((response) => setGroup(response.data))
       .catch((err) => console.log(err));
   };
 
   const createGroup = (data) => {
-    Api.post("groups/", data)
+    Api.post("/groups/", data, {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(
+          localStorage.getItem("@StriveToGet: Token" || "")
+        )}`,
+      },
+    })
       .then(toast.success("Group Created!"))
       .catch((err) => console.log(err));
   };
 
   const editGroup = (data) => {
-    Api.patch(`groups/${data.id}/`, data)
+    Api.patch(`/groups/${data.id}/`, data, {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(
+          localStorage.getItem("@StriveToGet: Token" || "")
+        )}`,
+      },
+    })
       .then(toast.success("Group edited!"))
       .catch((err) => console.log(err));
   };
 
   const groupSubscription = (data) => {
-    Api.post(`groups/${data.id}/subscribe/`)
+    Api.post(`/groups/${data.id}/subscribe/`, "", {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(
+          localStorage.getItem("@StriveToGet: Token" || "")
+        )}`,
+      },
+    })
       .then(toast.success("Subscribed!"))
       .catch((err) => console.log(err));
   };
 
   const buscaSubs = () => {
-    Api.get(`groups/subscriptions/`)
-      .then(toast.success("Subed Groups"))
+    Api.get(`/groups/subscriptions/`, {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(
+          localStorage.getItem("@StriveToGet: Token" || "")
+        )}`,
+      },
+    })
+      .then((res) => setGroupsInscribed(res.data))
       .catch((err) => console.log(err));
   };
 
   const exitGroup = (group) => {
-    Api.delete(`groups/${group.id}/unsubscribe/`)
+    Api.delete(`/groups/${group.id}/unsubscribe/`, {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(token)}`,
+      },
+    })
       .then(toast.success("Unsubscribed sucessufully"))
       .catch((err) => console.log(err));
   };
 
   return (
     <GroupsContext.Provider
-      value={
-        (groups,
+      value={{
+        groups,
+        groupsInscribed,
         group,
-        listGroups,
+        PerPage,
         listOneGroup,
         createGroup,
         editGroup,
         groupSubscription,
         buscaSubs,
-        exitGroup)
-      }
+        exitGroup,
+      }}
     >
       {children}
     </GroupsContext.Provider>
