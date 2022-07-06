5ff81d71cfbd50310c958a8bd9ffb9d67f3bcd32
Fernanda Constância <91707875+fernandaconstancia@users.noreply.github.com>
2022-07-05 22:57:07 -0300

    fix: RegisterGroup criação da função e ajuste cor do select

--- src/Components/RegisterGroup/index.jsx
+++ src/Components/RegisterGroup/index.jsx
@@ -1,30 +1,57 @@
 import * as S from "./style";
 import { FiX } from "react-icons/fi";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
 export const RegisterGroup = ({ onClose, onSubmit }) => {
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("Estudo");
+  const [description, setDescription] = useState("");
+
+  const onSubmitFunction = (e) => {
+    e.preventDefault();
+    const data = {
+      name,
+      category,
+      description,
+    };
+
+    if (!data.name) return toast.error("Por favor digite o nome do grupo!");
+
+    if (!data.description)
+      return toast.error("Por favor digite a descrição do grupo!");
+
+    onSubmit(data);
+    onClose();
+  };
+
   return (
     <S.Container>
       <S.Box>
-        <form>
+        <form onSubmit={onSubmitFunction}>
           <S.Header>
             <h1>Cadastrar Grupo</h1>
-            <S.Close onclick={onClose}>
+            <S.Close onClick={onClose}>
               <FiX />
             </S.Close>
           </S.Header>
           <S.Input>
             <label>Grupo:</label>
-            <input placeholder="Digite aqui o nome do seu grupo" />
+            <input
+              placeholder="Digite aqui o nome do seu grupo"
+              onChange={(e) => setName(e.target.value)}
+            />
           </S.Input>
           <S.Select>
             <label>Categoria:</label>
-            <select>
+            <select onChange={(e) => setCategory(e.target.value)}>
               <option value="" disabled>
                 Selecione a categoria
               </option>
-              <option value="study">Estudo</option>
-              <option value="health">Saúde</option>
-              <option value="work">Trabalho</option>
+              <option value="Estudo">Estudo</option>
+              <option value="Saúde">Saúde</option>
+              <option value="Trabalho">Trabalho</option>
+              <option value="Entretenimento">Entretenimento</option>
             </select>
           </S.Select>
           <S.Textarea>
@@ -35,6 +62,7 @@ export const RegisterGroup = ({ onClose, onSubmit }) => {
               cols="20"
               rows="5"
               placeholder="Digite aqui a descrição do seu grupo"
+              onChange={(e) => setDescription(e.target.value)}
             />
           </S.Textarea>
           <S.Button onClick={onSubmit}>
