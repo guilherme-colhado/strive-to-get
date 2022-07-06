ce8742a6fb045e27bc2dde4f993c5c7356fd375f
ibarbarasouza <ibarbarasouza@icloud.com>
2022-07-05 21:18:55 -0300

    feat: Info ajuste no style

--- src/Components/Info/index.jsx
+++ src/Components/Info/index.jsx
@@ -1,21 +1,23 @@
-import { Container } from "./style";
+import { Container, Card } from "./style";
 
 export const CardInfo = () => {
   return (
     <Container>
-      <ul>
-        <li>Ajuda na criação de hábitos;</li>
-        <li>
-          Permite que você tenha controle do seu tempo gasto durante o dia;
-        </li>
-        <li>Maior organização para montar um plano de tarefas;</li>
-        <li>Lista de hábitos almejados organizados em grupos;</li>
-        <li>
-          Desbloqueio de bônus de tempo para usar no seu hobby favorito, após
-          cumprir tarefas diárias;
-        </li>
-        <li>Interface interativa.</li>
-      </ul>
+      <Card>
+        <ul>
+          <li>Ajuda na criação de hábitos;</li>
+          <li>
+            Permite que você tenha controle do seu tempo gasto durante o dia;
+          </li>
+          <li>Maior organização para montar um plano de tarefas;</li>
+          <li>Lista de hábitos almejados organizados em grupos;</li>
+          <li>
+            Desbloqueio de bônus de tempo para usar no seu hobby favorito, após
+            cumprir tarefas diárias;
+          </li>
+          <li>Interface interativa.</li>
+        </ul>
+      </Card>
     </Container>
   );
 };
--- src/Components/Info/style.js
+++ src/Components/Info/style.js
@@ -1,7 +1,21 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+<<<<<<< Updated upstream
   width: 500px;
+=======
+  max-width: 600px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+`;
+
+export const Card = styled.div`
+  width: 100%;
+  height: 503px;
+>>>>>>> Stashed changes
   background-color: var(--CardDark);
   border: none;
   border-radius: 8px;
@@ -19,9 +33,18 @@ export const Container = styled.div`
     margin-bottom: 15px;
   }
 
+<<<<<<< Updated upstream
   @media (max-width: 720px) {
     font-size: 16px;
     width: 80%;
     padding: 30px;
+=======
+  @media (max-width: 768px) {
+    font-size: 23px;
+  }
+
+  @media (max-width: 360px) {
+    font-size: 19px;
+>>>>>>> Stashed changes
   }
 `;
