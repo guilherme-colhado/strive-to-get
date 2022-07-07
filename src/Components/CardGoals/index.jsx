8a1c1869fab942b9aecc220c4eef8f212895cc4e
Galvs135 <lg_franca@hotmail.com>
2022-07-07 03:36:35 -0300

    fix: feita correção nos titulos dos cards

--- src/Components/CardGoals/index.jsx
+++ src/Components/CardGoals/index.jsx
@@ -2,30 +2,39 @@ import { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { GoalsContext } from "../../Providers/Goals";
 import { CardStyle, HeaderCard, InfosCard } from "./style";
-import { useState } from 'react'
-import { Modal } from '../Modal'
-import { ComfirmAchieved } from '../ComfirmAchieved'
+import { useState } from "react";
+import { Modal } from "../Modal";
+import { ComfirmAchieved } from "../ComfirmAchieved";
 
 export const CardGoals = ({ goals }) => {
   const { updateGoals } = useContext(GoalsContext);
-  const {id} = useParams() 
-  const [ modal, setModal ] = useState(false)
+  const { id } = useParams();
+  const [modal, setModal] = useState(false);
   return (
-    !goals.achieved && <>
-      <CardStyle>
-        <HeaderCard>{goals.title}</HeaderCard>
-        <InfosCard>
-          <span>Dificuldade: {goals.difficulty} </span>
-          <button onClick={()=> setModal(true)}>Arquivar</button>
-        </InfosCard>
-      </CardStyle>
-      <Modal open={modal}>
-        <ComfirmAchieved onClose={()=>setModal(false)} onSubmit={()=>{
-          updateGoals(goals, id)
-        }}>
-          Você tem certeza que quer arquivar esta atividade?
-        </ComfirmAchieved>
-      </Modal>
-    </> 
+    !goals.achieved && (
+      <>
+        <CardStyle>
+          <HeaderCard>
+            {goals.title.length >= 9
+              ? `${goals.title.slice(0, 9).concat("...")}`
+              : goals.title}
+          </HeaderCard>
+          <InfosCard>
+            <span>Dificuldade: {goals.difficulty} </span>
+            <button onClick={() => setModal(true)}>Arquivar</button>
+          </InfosCard>
+        </CardStyle>
+        <Modal open={modal}>
+          <ComfirmAchieved
+            onClose={() => setModal(false)}
+            onSubmit={() => {
+              updateGoals(goals, id);
+            }}
+          >
+            Você tem certeza que quer arquivar esta atividade?
+          </ComfirmAchieved>
+        </Modal>
+      </>
+    )
   );
 };
